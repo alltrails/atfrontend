e22bd6d59697163a510e0499640aa6b5a2794d5e
electricjug <bikinivoodoodrummer@gmail.com>
2020-12-11 18:48:10 -0800

    added icons yay

--- src/components/restaurant/index.jsx
+++ src/components/restaurant/index.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './style.scss';
+import { FaHeart, FaRegHeart, FaStar, FaRegStar } from 'react-icons/fa';
 
+// when selecting a heart switch to FaHeart and add class .green
+// when rendering the stars, switch to FaStar and add class .gold
 class Restaurant extends Component {
   render() {
     return (
@@ -8,10 +11,22 @@ class Restaurant extends Component {
         <img src="https://4fi8v2446i0sw2rpq2a3fg51-wpengine.netdna-ssl.com/wp-content/uploads/2014/06/About_PhotoBanners_344x361.jpg" />
         <div className="restaurantInfo">
           <h4>Restaurant name</h4>
-          <p><span className="stars">stars</span><span className="votes">(123)</span></p>
+          <p className="rating">
+            <span className="stars">
+              <FaRegStar />
+              <FaRegStar />
+              <FaRegStar />
+              <FaRegStar />
+              <FaRegStar />
+            </span>
+            <span className="votes">(123)</span></p>
           <p><span className="dollars">$$</span> &#183; <span className="notes">Supporting text</span></p>
         </div>
-        <div className="favorite">heart</div>
+        <div className="favorite">
+          <a href="#">
+            <FaRegHeart />
+          </a>
+        </div>
       </div>
     )
   }
